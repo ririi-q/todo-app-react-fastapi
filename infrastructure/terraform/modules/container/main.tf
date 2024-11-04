1eda652de2c8492b7b3c99e497f7e45d13b44801
林完多 <ririi_q@mypc.local>
2024-11-05 00:46:02 +0900

    Develop 241105:0046

--- infrastructure/terraform/modules/container/main.tf
+++ infrastructure/terraform/modules/container/main.tf
@@ -125,35 +125,36 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
-  default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
   # default_action {
-  #   type             = "forward"
-  #   target_group_arn = aws_lb_target_group.backend.arn
+  #   type = "redirect"
+  #   redirect {
+  #     port        = "443"
+  #     protocol    = "HTTPS"
+  #     status_code = "HTTP_301"
+  #   }
   # }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.certificate_arn
-
-  # 証明書が完全に有効になるまで待機
-  depends_on = [var.certificate_arn]
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.backend.arn
   }
-  
 }
+
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = aws_lb.main.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = var.certificate_arn
+
+#   # 証明書が完全に有効になるまで待機
+#   depends_on = [var.certificate_arn]
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.backend.arn
+#   }
+  
+# }
+
 # ECS Task Definition
 resource "aws_ecs_task_definition" "backend" {
   family                   = "${var.project}-${var.env}-backend"
@@ -196,13 +197,13 @@ resource "aws_ecs_task_definition" "backend" {
         ]
       )
 
-      # healthCheck = {
-      #   command     = ["CMD", "curl", "-f", "http://localhost:8000/health"]
-      #   interval    = 30
-      #   timeout     = 5
-      #   retries     = 3
-      #   startPeriod = 60
-      # }
+      healthCheck = {
+        command     = ["CMD", "curl", "-f", "http://localhost:8000/health"]
+        interval    = 30
+        timeout     = 5
+        retries     = 3
+        startPeriod = 60
+      }
 
       portMappings = [
         {
